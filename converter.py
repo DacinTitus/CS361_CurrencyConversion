d5ce7590957772e3f58fbebf9eab0530656e02de
DacinTitus <titusda@oregonstate.edu>
2022-10-31 10:53:21 -0700

    first commit

---
+++ converter.py
@@ -0,0 +1,53 @@
+# This will read from the convert.txt file
+# Then make the conversion and write to converted.txt file
+
+from forex_python.converter import CurrencyRates
+from forex_python.converter import CurrencyCodes
+
+# c = CurrencyRates()
+# print(c.get_rate('USD', 'INR'))
+# print(c.convert('USD', 'INR', 10.50))
+
+if __name__ == '__main__':
+
+    while True:
+        file_convert = open('convert.txt', 'r')
+        line = file_convert.readline()
+        file_convert.close()
+
+        if not line:
+            continue
+
+        else:
+            file_convert = open('convert.txt', 'r')
+            convert_tuple = ()
+
+            # the first line will be the number of items to put back in the tuple
+            number_elements = int(file_convert.readline())
+
+            i = 0
+            while i < number_elements:
+                element = file_convert.readline()
+                if (element != ''):
+                    if i < 2:
+                        element = element.replace('\n', '')
+                        convert_tuple = convert_tuple + (str(element),)
+                    else:
+                        convert_tuple = convert_tuple + (float(element),)
+                i += 1
+        
+            file_convert.close()
+
+        # test prints
+        # print('currency tuple: ')
+        # print(convert_tuple)
+
+        # convert amount and write to converted.txt
+            c = CurrencyRates()
+            data = c.convert(convert_tuple[0], convert_tuple[1], convert_tuple[2])
+
+            file_converted = open('converted.txt', 'w')
+            print(data)
+            file_converted.write(str(data))
+            file_converted.close()
+
---
+++ index.py
@@ -0,0 +1,37 @@
+# code from index will need to be added to some page of project
+
+from forex_python.converter import CurrencyRates
+from forex_python.converter import CurrencyCodes
+
+c = CurrencyRates()
+print(c.get_rate('USD', 'INR'))
+print(c.convert('USD', 'INR', 10.50))
+
+# app sends request in the form of a tuple as in ('USD', 'USR', 10)
+# first element is FROM, second element is TO, and third element is AMOUNT
+
+convert_from = 'USD'
+convert_to = 'INR'
+convert_amount = 10
+
+my_tuple = (convert_from, convert_to, convert_amount)
+
+# write what is to be converted to convert.txt
+file_convert = open('convert.txt', 'w')
+file_convert.write(str(len(my_tuple)) + '\n')
+
+for item in my_tuple:
+    file_convert.write(str(item) + '\n')
+file_convert.close()
+
+
+# read converted amount from converted.txt and convert to int
+file_converted = open('converted.txt', 'r')
+new_amount = file_converted.readline()
+
+new_amount = float(new_amount)
+new_amount = format(new_amount, '.2f')   # limit to two decimal places
+file_converted.close()
+
+# test print
+print(new_amount)
